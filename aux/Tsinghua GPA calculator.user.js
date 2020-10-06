c14f5563cf801e21508a3cb2005f52d6cb47ee4a
SuXY15 <suxy15tsinghua@gmail.com>
2020-10-06 16:19:50 +0800

    modify GPA calculator to be suitable for graduate students

--- aux/Tsinghua GPA calculator.user.js
+++ aux/Tsinghua GPA calculator.user.js
@@ -2,82 +2,66 @@
 // @name         Tsinghua GPA calculator
 // @namespace    https://github.com/ZenithalHourlyRate
 // @homepageURL  https://github.com/ZenithalHourlyRate/thuservices
-// @version      0.1
+// @version      1.0
 // @description  Calculate the gpa on page
 // @author       Zenithal
 // @match        http://zhjw.cic.tsinghua.edu.cn/cj.cjCjbAll.do?m=*_yxkccj*
 // @grant        none
 // ==/UserScript==
 
-function old_gpa_convert(g){
-    switch(g){
-        case 'A+':return 40;
-        case 'A':return 40;
-        case 'A-':return 37;
-        case 'B+':return 33;
-        case 'B':return 30;
-        case 'B-':return 27;
-        case 'C+':return 23;
-        case 'C':return 20;
-        case 'C-':return 17;
-        case 'D+':return 13;
-        case 'D':return 10;
-        case 'F':return 0;
-        default:return -1;
-    }
-}
+// from https://stackoverflow.com/questions/6600868
+var handler = {
+  get: function(target, name) {
+    return target.hasOwnProperty(name) ? target[name] : -1;
+  }
+};
 
-function new_gpa_convert(g){
-    switch(g){
-        case 'A+':return 40;
-        case 'A':return 40;
-        case 'A-':return 40;
-        case 'B+':return 36;
-        case 'B':return 33;
-        case 'B-':return 30;
-        case 'C+':return 26;
-        case 'C':return 23;
-        case 'C-':return 20;
-        case 'D+':return 16;
-        case 'D':return 13;
-        case 'F':return 0;
-        default:return -1;
-    }
-}
+var old_gpa_dict = new Proxy({
+    'A+': 4.0, 'A': 4.0, 'A-': 3.7,
+    'B+': 3.3, 'B': 3.0, 'B-': 2.7,
+    'C+': 2.3, 'C': 2.0, 'C-': 1.7,
+    'D+': 1.3, 'D': 1.0, 'F':  0
+}, handler);
+
+var new_gpa_dict = new Proxy({
+    'A+': 4.0, 'A': 4.0, 'A-': 4.0,
+    'B+': 3.6, 'B': 3.3, 'B-': 3.0,
+    'C+': 2.6, 'C': 2.3, 'C-': 2.0,
+    'D+': 1.6, 'D': 1.3, 'F':  0
+}, handler);
 
 (function() {
     var table = document.getElementsByClassName('table')[0];
-    var oldSum=0,newSum=0,credit=0;
-    var rOldSum=0,rNewSum=0,rCredit=0; // Required and Restricted selective
-    for(var i=1;i<table.rows.length-1;++i){ // skip the first and last row
+    var oldSum=0, newSum=0, credit=0;
+    var rOldSum=0, rNewSum=0, rCredit=0; // Required and Restricted selective
+
+    for(var i=1; i<table.rows.length; ++i){ // skip the first row
         var r = table.rows[i]
-        if(r.cells.length != 12) {
+        if(r.cells.length != 12) { // make sure current row has course data
             continue;
         }
         var c = parseInt(r.cells[3].innerText);
         var g = r.cells[5].innerText;
-        var o = old_gpa_convert(g);
-        var n = new_gpa_convert(g);
-
-        if(o!=-1){
-            oldSum+=c*o;
-            newSum+=c*n;
-            credit+=c;
+        var o = old_gpa_dict[g];
+        var n = new_gpa_dict[g];
+        if(o != -1){
+            oldSum += c*o;
+            newSum += c*n;
+            credit += c;
             if(r.cells[8].innerText=="必修" || r.cells[8].innerText=="限选"){
-                rOldSum+=c*o;
-                rNewSum+=c*n;
-                rCredit+=c;
+                rOldSum += c*o;
+                rNewSum += c*n;
+                rCredit += c;
             }
         }
-
     }
 
-
-    var s =
-        "New " + (newSum/credit/10).toString() + "\n" +
-        "Old " + (oldSum/credit/10).toString() + "\n" +
-        "R New " + (rNewSum/rCredit/10).toString() + "\n" +
-        "R Old " + (rOldSum/rCredit/10).toString();
+    var s = "New " + (newSum/credit).toString() + "\n" +
+            "Old " + (oldSum/credit).toString() + "\n";
+    if(rCredit > 0){ // there is no "必修"/"限选" for graduate students
+        s+= "R New " + (rNewSum/rCredit).toString() + "\n" +
+            "R Old " + (rOldSum/rCredit).toString();
+    }
 
     alert(s);
 })();
