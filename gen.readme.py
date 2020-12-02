861675f2fff597008420f10958c348639d514e63
Zenithal <i@zenithal.me>
2020-12-02 22:15:35 +0800

    Update gen.readme to avoid shebang

--- gen.readme.py
+++ gen.readme.py
@@ -8,5 +8,9 @@ for f in files:
     with open(f,'r') as fp:
         print('- [{}]({})'.format(f,f))
         for line in fp:
-            if '#' in line[0]:
+            if '#' in line[0] and '# ' in line:
+                # first we need to guanantee the line begin with #, as section title
+                # then we need to avoid some cases such as shebang
+                # FIXME: comment line in code begin with '# ' may still be added
+                # into README, we may skip code blocks to solve this issue
                 print(line.rstrip('\n').replace('# ','#- ').replace('#','    '))
